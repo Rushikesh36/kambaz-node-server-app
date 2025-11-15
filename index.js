85913307aa44136bc68e7fe51af5dd2146561934
Rushikesh Wani <rushikeshwani36@gmail.com>
2025-11-14 21:01:45 -0500

    done till 5.3.3.1 till .env page 190

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from 'express'
 import Hello from "./Hello.js";
 import Lab5 from "./lab5/index.js";
+import db from "./Kambaz/Database/index.js";
+import UserRoutes from "./Kambaz/Users/routes.js";
 import cors from "cors";
 const app = express();
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
+}));
 app.use(express.json());
+UserRoutes(app, db);
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000);
