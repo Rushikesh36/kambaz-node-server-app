1299bca782f91198f200708d4635c23c96b47698
Rushikesh Wani <rushikeshwani36@gmail.com>
2025-11-16 01:51:58 -0500

    done before 5.3.3 page 199

--- index.js
+++ index.js
@@ -3,13 +3,33 @@ import Hello from "./Hello.js";
 import Lab5 from "./lab5/index.js";
 import db from "./Kambaz/Database/index.js";
 import UserRoutes from "./Kambaz/Users/routes.js";
+import CourseRoutes from "./Kambaz/Courses/routes.js";
 import cors from "cors";
+import "dotenv/config";
+import session from "express-session";
+
 const app = express();
 app.use(cors({
     credentials: true,
     origin: process.env.CLIENT_URL || "http://localhost:3000",
 }));
+
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kambaz",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.SERVER_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+        domain: process.env.SERVER_URL,
+    };
+}
+app.use(session(sessionOptions));
 app.use(express.json());
+CourseRoutes(app, db);
 UserRoutes(app, db);
 Lab5(app);
 Hello(app);
