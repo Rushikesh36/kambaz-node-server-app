a925c41a335c9b866df8ac3253407ae623e823ae
Rushikesh Wani <rushikeshwani36@gmail.com>
2025-11-19 19:57:07 -0500

    done till 6.4.1

--- Kambaz/Courses/routes.js
+++ Kambaz/Courses/routes.js
@@ -1,41 +1,41 @@
 import CoursesDao from "./dao.js";
 
-export default function CourseRoutes(app, db) {
+export default function CourseRoutes(app) {
     const dao = CoursesDao(db);
 
-    const createCourse = (req, res) => {
+    const createCourse = async (req, res) => {
         const currentUser = req.session["currentUser"];
         if (!currentUser) {
             res.sendStatus(401);
             return;
         }
-        const newCourse = dao.createCourse(req.body);
+        const newCourse = await dao.createCourse(req.body);
         res.json(newCourse);
     };
     app.post("/api/users/current/courses", createCourse);
 
-    const deleteCourse = (req, res) => {
+    const deleteCourse = async (req, res) => {
         const { courseId } = req.params;
-        const status = dao.deleteCourse(courseId);
+        const status = await dao.deleteCourse(courseId);
         res.send(status);
     };
     app.delete("/api/courses/:courseId", deleteCourse);
 
-    const updateCourse = (req, res) => {
+    const updateCourse = async (req, res) => {
         const { courseId } = req.params;
         const courseUpdates = req.body;
-        const status = dao.updateCourse(courseId, courseUpdates);
+        const status = await dao.updateCourse(courseId, courseUpdates);
         res.send(status);
     };
     app.put("/api/courses/:courseId", updateCourse);
 
-    const findAllCourses = (req, res) => {
-        const courses = dao.findAllCourses();
+    const findAllCourses = async (req, res) => {
+        const courses = await dao.findAllCourses();
         res.send(courses);
     };
     app.get("/api/courses", findAllCourses);
 
-    const findCoursesForEnrolledUser = (req, res) => {
+    const findCoursesForEnrolledUser = async (req, res) => {
         let { userId } = req.params;
         if (userId === "current") {
             const currentUser = req.session["currentUser"];
@@ -45,7 +45,7 @@ export default function CourseRoutes(app, db) {
             }
             userId = currentUser._id;
         }
-        const courses = dao.findCoursesForEnrolledUser(userId);
+        const courses = await dao.findCoursesForEnrolledUser(userId);
         res.json(courses);
     };
     app.get("/api/users/:userId/courses", findCoursesForEnrolledUser);
